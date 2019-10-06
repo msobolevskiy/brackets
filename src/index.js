64c14a42096aa6e2bcc2b664130c66bbc1d26de7
maximsobolevskiy <maximsob1233@gmail.com>
2019-10-06 23:59:20 +0300

    done task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  while (str.length > 0) {
+    previousStr = str;
+    str = clining(str, bracketsConfig);
+
+    if (str === previousStr) {
+        return false;
+    } 
+}
+return true;
+}
+
+function clining(str, bracketsConfig) {
+    bracketsConfig.forEach(elem => {
+        part = elem.join('')
+        str = str.replace(part, '')
+    });
+    return str;
 }
